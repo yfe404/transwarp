776b801802172737a7db96b6fa3c0b66f819dfff
yafeunteun <yfe.github@protonmail.com>
2020-07-12 17:08:43 +0200

    multithreading WIP

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ import requests
 import urllib.parse
 import youtube_dl
 import json
-from retrying import retry
+
+from queue import Queue
+from threading import Thread
 
 from config import youtube_config
 
@@ -16,6 +18,48 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger('main')
+
+
+class Worker(Thread):
+    """ Thread executing tasks from a given task queue """
+
+    def __init__(self, tasks):
+        Thread.__init__(self)
+        self.tasks = tasks
+        self.daemon = True
+        self.start()
+
+    def run(self):
+        while True:
+            func, args = self.tasks.get()
+            try:
+                func(*args)
+            except Exception as error:
+                # An exception happened while executing
+                print(error)
+            finally:
+                # Mark this task as done, whether an exception happened or not
+                self.tasks.task_done()
+
+
+class ThreadPool:
+    """ Pool of threads consuming tasks from a queue """
+
+    def __init__(self, num_threads=10):
+        self.tasks = Queue(num_threads)
+        for _ in range(num_threads):
+            Worker(self.tasks)
+
+    def map(self, func, arg_list):
+        """ Add a list of tasks to the queue """
+        for args in arg_list:
+            self.tasks.put((func, args))
+
+    def wait_completion(self):
+        """ Wait for completion of all tasks in the queue """
+        self.tasks.join()
+
+
 
 
 def find_song_url(query):
@@ -39,7 +83,7 @@ def find_song_url(query):
 
     data = f"category_music=1&{urllib.parse.urlencode({'q': query})}&pageno=1&time_range=None&language=en-US&format=json"
 
-    url = "https://search.fuckoffgoogle.net/"  ## @todo replace by a config['SEARX_INSTANCE']
+    url = "http://127.0.0.1:7777/"  ## @todo replace by a config['SEARX_INSTANCE']
     response = requests.get(url, data, headers=headers).json()
     for result in response["results"]:
         #title, url, content, host, engine, score, _type = line.split(",")
@@ -49,37 +93,43 @@ def find_song_url(query):
     return None
 
 
+def get_mp3(query):
+    #logger.info(f"Downloading {song[1]} from {song[0]}")
+    logger.info(f"Downloading {query}")
+    song_url = find_song_url(query)
+
+    pos = song_url.find("&list")
+    if pos != -1:
+        song_url = song_url[:pos]
+    try:
+        ydl_opts = youtube_config.get_youtube_config()
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([song_url])
+
+    except Exception as e:
+        print(e)  # @todo log error
+
+
 def check_playlist_file(path):#@todo
     logger.info(f"Checking and removing any duplicate tracks in {path}")
     return True
 
-check_playlist_file("./example_song_list.txt")
 
-with open("./example_song_list.txt", "r") as f:
-#INFO: Downloading 67 tracks.
 
-#INFO: 1. Downloading "https://open.spotify.com/track/0sCeNwt8xRCMR4NhKpMyBe"
+if __name__ == "__main__":
 
-    songs = []
-    for line in f.readlines():
-        title, artist = line.strip().split("|")
-        songs.append((title, artist))
+    check_playlist_file("./example_song_list.txt")
 
+    queries = []
+    with open("./example_song_list.txt", "r") as f:
+
+        songs = []
+        for line in f.readlines():
+            title, artist = line.strip().split("|")
+            song = (title, artist)
+            queries.append(f"{song[0]} {song[1]}")
     logger.info(f"Downloading {len(songs)} tracks.")
 
-    for idx, song in enumerate(songs):
-        query = f"{song[0]} {song[1]}"
-        logger.info(f"{idx+1}. Downloading {song[1]} from {song[0]}")
-
-        song_url = find_song_url(query)
-        pos = song_url.find("&list")
-        if pos != -1:
-            song_url = song_url[:pos]
-
-        try:
-            ydl_opts = youtube_config.get_youtube_config()
-            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                ydl.download([song_url])
-
-        except Exception as e:
-            print(e)  # @todo log error
+    pool = ThreadPool(5)
+    pool.map(get_mp3, [(x,) for x in queries])
+    pool.wait_completion()
