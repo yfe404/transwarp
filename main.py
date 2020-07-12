cd708c857ff1ac6b2e9e866092ded98f26b35c13
yafeunteun <yfe.github@protonmail.com>
2020-07-12 11:48:37 +0200

    change searx instance, fuckoffgoogle is <3

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ logging.basicConfig(
 )
 
 
-
 def find_song_url(query):
     """
     Use Selenium to go to YouTube Music and search for a song
@@ -24,6 +23,8 @@ def find_song_url(query):
     :param query: The query describing the song to look for (exemple artist and title)
     :return: The URL of the song on YouTube Music to be sent to youtube-dl
     """
+
+
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
@@ -31,15 +32,20 @@ def find_song_url(query):
         'Accept-Language': 'en-US,en;q=0.5',
         'Origin': 'null',
         'Connection': 'keep-alive',
-        "Cookie": "results_on_new_tab=0; safesearch=0; categories=general; oscar-style=logicodev; language=en-US; image_proxy=1; autocomplete=; theme=oscar; method=POST; disabled_engines=\"deezer__music\054torrentz__music\054mixcloud__music\054seedpeer__music\054invidious__music\054genius__music\054piratebay__music\054soundcloud__music\054btdigg__music\"; enabled_engines=; disabled_plugins=; enabled_plugins='",
+        "Cookie": "results_on_new_tab=0; safesearch=0; categories='music\054general'; oscar-style=logicodev; language=en-US; image_proxy=1; autocomplete=; theme=fuckoffgoogle; method=POST; disabled_engines=\"deezer__music\054torrentz__music\054mixcloud__music\054seedpeer__music\054invidious__music\054genius__music\054piratebay__music\054soundcloud__music\054btdigg__music\"; enabled_engines=; disabled_plugins=; enabled_plugins='",
         'Upgrade-Insecure-Requests': '1'
     }
 
-    data = f"category_music=1&{urllib.parse.urlencode({'q': query})}&pageno=1&time_range=None&language=en-US&format=json"
+    data = f"category_music=1&{urllib.parse.urlencode({'q': query})}&pageno=1&time_range=None&language=en-US&format=csv"
 
-    url = "https://searx.pofilo.fr/" ## @todo replace by a config['SEARX_INSTANCE']
-    r = requests.get(url, data, headers=headers)
-    return r.json()["results"][0]["url"]
+    url = "https://search.fuckoffgoogle.net/" ## @todo replace by a config['SEARX_INSTANCE']
+    response = requests.get(url, data, headers=headers)
+    for line in  response.text.split("\n"):
+        title,url,content,host,engine,score,_type = line.split(",")
+        if engine == "www.youtube.com": #@todo and score > threshold
+            return url
+
+    return None
 
 with open("./example_song_list.txt", "r") as f:
     songs = []
