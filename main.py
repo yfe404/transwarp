714fa3999971e00818e31dde428f1265e8a3e197
yafeunteun <yfe.github@protonmail.com>
2020-07-12 11:57:03 +0200

    formatting

--- main.py
+++ main.py
@@ -24,28 +24,28 @@ def find_song_url(query):
     :return: The URL of the song on YouTube Music to be sent to youtube-dl
     """
 
-
     headers = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Accept-Language': 'en-US,en;q=0.5',
-        'Origin': 'null',
-        'Connection': 'keep-alive',
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Origin": "null",
+        "Connection": "keep-alive",
         "Cookie": "results_on_new_tab=0; safesearch=0; categories='music\054general'; oscar-style=logicodev; language=en-US; image_proxy=1; autocomplete=; theme=fuckoffgoogle; method=POST; disabled_engines=\"deezer__music\054torrentz__music\054mixcloud__music\054seedpeer__music\054invidious__music\054genius__music\054piratebay__music\054soundcloud__music\054btdigg__music\"; enabled_engines=; disabled_plugins=; enabled_plugins='",
-        'Upgrade-Insecure-Requests': '1'
+        "Upgrade-Insecure-Requests": "1",
     }
 
     data = f"category_music=1&{urllib.parse.urlencode({'q': query})}&pageno=1&time_range=None&language=en-US&format=csv"
 
-    url = "https://search.fuckoffgoogle.net/" ## @todo replace by a config['SEARX_INSTANCE']
+    url = "https://search.fuckoffgoogle.net/"  ## @todo replace by a config['SEARX_INSTANCE']
     response = requests.get(url, data, headers=headers)
-    for line in  response.text.split("\n"):
-        title,url,content,host,engine,score,_type = line.split(",")
-        if engine == "www.youtube.com": #@todo and score > threshold
+    for line in response.text.split("\n"):
+        title, url, content, host, engine, score, _type = line.split(",")
+        if engine == "www.youtube.com":  # @todo and score > threshold
             return url
 
     return None
+
 
 with open("./example_song_list.txt", "r") as f:
     songs = []
